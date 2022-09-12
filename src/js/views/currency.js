b15cb3a96b652708e3f699ad9589f94d2cd332aa
mikhailkushkov <mikhailkushkov@gmail.com>
2022-09-12 11:46:28 +0200

    initial commit

---
+++ src/js/views/currency.js
@@ -0,0 +1,22 @@
+class CurrencyUI {
+  constructor() {
+    this.currency = document.getElementById('currency');
+    this.dictionary = {
+      USD: '$',
+      EUR: '€',
+      UAH: '₴',
+    };
+  }
+
+  get currecyValue() {
+    return this.currency.value;
+  }
+
+  getCurrencySymbol() {
+    return this.dictionary[this.currecyValue];
+  }
+}
+
+const currencyUI = new CurrencyUI();
+
+export default currencyUI;
